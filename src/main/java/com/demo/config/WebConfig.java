e777bbef31f37aba11b9b3ffc5daa4b1ac89080d
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-07-31 10:49:49 +0200

    Instantiate MVC and Register Servlet

---
+++ src/main/java/com/demo/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+// import spring mvc configuration
+@EnableWebMvc
+@Configuration
+@Component
+public class WebConfig {
+}
