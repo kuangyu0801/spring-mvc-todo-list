2e0556face1bd63ef0ccc4c511ad85413d0d8eaa
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-07-31 13:47:17 +0200

    Add Model Attribute

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -1,10 +1,14 @@
 package com.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 // controller is a special type of component annotation
+@Slf4j
 @Controller
 public class DemoController {
 
@@ -19,9 +23,17 @@ public class DemoController {
 
     // http://localhost:8080/todo-list-mvc/welcome
     @GetMapping("welcome")
-    public String welcome() {
+    public String welcome(Model model) {
+        model.addAttribute("user", "Tim");
+        log.info("model= {}", model);
         // "welcome" is a view name: ie jsp file
         // WEB-INF/view/welcome.jsp will be generated
         return "welcome";
+    }
+
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage() {
+        log.info("welcomeMessage() called");
+        return "Welcome to this Demo application.";
     }
 }
--- src/main/webapp/WEB-INF/view/welcome.jsp
+++ src/main/webapp/WEB-INF/view/welcome.jsp
@@ -11,6 +11,7 @@
     <title>Welcome</title>
 </head>
 <body>
-    <h1>Welcome Hello World</h1>
+    <h1>Welcome ${user}</h1>
+    <h2>${welcomeMessage}</h2>
 </body>
 </html>
