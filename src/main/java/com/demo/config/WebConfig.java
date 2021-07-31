91970726c33bf01db42d84c15b7fed6c856cbb09
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-07-31 19:36:22 +0200

    Add Home page and redirect page to item list with delete

--- src/main/java/com/demo/config/WebConfig.java
+++ src/main/java/com/demo/config/WebConfig.java
@@ -1,10 +1,13 @@
 package com.demo.config;
 
+import com.demo.util.ViewNames;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
@@ -12,7 +15,7 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "com.demo")
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     // == constants ==
     public static final String RESOLVER_PREFIX = "WEB-INF/view/";
@@ -24,5 +27,11 @@ public class WebConfig {
         viewResolver.setPrefix(RESOLVER_PREFIX);
         viewResolver.setSuffix(RESOLVER_SUFFIX);
         return viewResolver;
+    }
+
+    // overriding the default mvc setting
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName(ViewNames.HOME);
     }
 }
--- src/main/webapp/WEB-INF/view/items_list.jsp
+++ src/main/webapp/WEB-INF/view/items_list.jsp
@@ -8,23 +8,35 @@
 <%-- using jstl tag --%>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="com.demo.util.Mappings" %>
+
 <html>
 <head>
     <title>Todo Items</title>
 </head>
 <body>
     <div align="center">
+    <c:url var="addUrl" value="${Mappings.ADD_ITEM}"></c:url>
+        <a href="${addUrl}">New Item</a>
         <table border="1" cellpadding="5">
             <caption><h2>Todo Items</h2></caption>
             <tr>
                 <th>Title</th>
                 <th>Deadline</th>
+                <th>Delete</th>
             </tr>
 
             <c:forEach var="item" items="${todoData.items}">
+
+                <c:url var="deleteUrl" value="${Nappings.DELETE_ITEM}">
+                    <c:param name="id" value="{item.id}"></c:param>
+                </c:url>
+
                 <tr>
                     <td><c:out value="${item.title}"/></td>
                     <td><c:out value="${item.deadline}"/></td>
+                        <%-- add delete buttom that redirect to Delete page --%>
+                    <td><a href="${deleteUrl}">Delete</a></td>
                 </tr>
             </c:forEach>
         </table>
